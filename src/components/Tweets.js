ffa91d1466228326dd7a2b0007c53a3d917c2c14
Luis montoya <luishm2024@gmail.com>
2021-11-16 16:02:40 -0500

    prueba hygeia

--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -1,48 +1,39 @@
-import React from 'react'
-import { View, Text,ScrollView,StyleSheet ,Image} from 'react-native'
+import React from 'react';
+import {View, Text, ScrollView, StyleSheet, Image} from 'react-native';
 
 export default function Tweets({data}) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.tweet}>
-                <View>
-                    <Text style={styles.titulo}>
-                      Usuario :  
-                    </Text>
-                    <Text>{data?.user?.name}</Text>
-                </View>
-                    <View>
-                        <Text style={styles.titulo}>
-                      Tweet: 
-                        </Text>
-                        <Text>
-                        {data?.text}
-
-                        </Text>
-                    </View>
-          </View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.tweet}>
+        <View>
+          <Text style={styles.titulo}>Usuario :</Text>
+          <Text>{data?.user?.name}</Text>
+        </View>
+        <View>
+          <Text style={styles.titulo}>Tweet:</Text>
+          <Text>{data?.text}</Text>
+        </View>
+      </View>
     </View>
-    )
+  );
 }
-
+/*Holi */
 const styles = StyleSheet.create({
-  
-    container:{
-        display:'flex',
-        flexDirection:'column',
-        borderWidth:1,
-        marginHorizontal:5,
-        borderColor:'blue',
-        marginVertical:10,
-        borderRadius:5
-
-    },
-    tweet:{
-        margin:5
-    },
-    titulo:{
-        color:'black',
-        fontSize:15,
-        fontWeight:'bold'
-    }
-})
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    borderWidth: 1,
+    marginHorizontal: 5,
+    borderColor: 'blue',
+    marginVertical: 10,
+    borderRadius: 5,
+  },
+  tweet: {
+    margin: 5,
+  },
+  titulo: {
+    color: 'black',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+});
